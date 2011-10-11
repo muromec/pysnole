e09086e98294fd8559f53c26435c3f843bcbf07d
Henning Schröder <henning.schroeder@gmail.com>
2011-10-11 08:28:09 +0200

    added setuptools

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+setup(
+    name = "pyqterm",
+    version = "0.1",
+    foo="bar",
+    packages = find_packages(),
+)
