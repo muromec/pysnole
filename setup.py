c64d7b70ea41c5c69682e1e4ced72f162b9dcae1
Henning Schröder <henning.schroeder@gmail.com>
2011-10-11 14:56:50 +0200

    added metadata to setup

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-setup(
-    name = "pyqterm",
-    version = "0.1",
-    foo="bar",
-    packages = find_packages(),
-)
+
+if __name__ == "__main__":
+    setup(
+        name = "pyqterm",
+        version = "0.1",
+        summary="Simple terminal/console widget for PyQt4 with vt100 support",
+        author="Henning Schroeder",
+        author_email="henning.schroeder@gmail.com",
+        packages = find_packages(),
+    )
